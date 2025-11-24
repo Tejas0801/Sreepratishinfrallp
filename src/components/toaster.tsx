a4343835e6ef8227a92f5a5064bad77b9bf982b5
Tejas0801 <tejakandhikatla97@gmail.com>
2025-11-24 12:16:16 +0530

    update

---
+++ src/components/toaster.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+// Minimal placeholder Toaster so build does not fail.
+// You can later replace this with the full shadcn Toaster implementation.
+export function Toaster() {
+  return null;
+}
+
+export default Toaster;
